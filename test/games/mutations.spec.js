695b52ffbf93de084d34e660f3b29167f446d191
raparicio6 <rodrigoaparicio6@gmail.com>
2020-04-11 22:45:47 -0300

    fixing play turn service failed test

--- test/games/mutations.spec.js
+++ test/games/mutations.spec.js
@@ -1,7 +1,17 @@
 const { mutate } = require('../server.test'),
   { createGame, playTurn } = require('./graphql'),
-  { mockCreateGame, mockPlayTurn, mockCreateGameRespondWithError } = require('../testUtils/mocks'),
-  { getGameExample, getGameWithTurnsExample, databaseError } = require('../testUtils/schemas/gamesSchemas');
+  {
+    mockCreateGame,
+    mockPlayTurn,
+    mockCreateGameRespondWithError,
+    mockPlayTurnRespondWithError
+  } = require('../testUtils/mocks'),
+  {
+    getGameExample,
+    getGameWithTurnsExample,
+    databaseError,
+    cardPlayedIsNotInHandError
+  } = require('../testUtils/schemas/gamesSchemas');
 
 describe('games', () => {
   describe('mutations', () => {
@@ -63,12 +73,12 @@ describe('games', () => {
     });
 
     it('Service respond with error, should respond with error', () => {
-      const playerName = 'Fred';
-      const statusCode = 503;
-      mockCreateGameRespondWithError(playerName, databaseError, statusCode);
-      return mutate(createGame({ game: { playerName } })).then(res => {
+      const gameId = 'abc123';
+      const statusCode = 400;
+      mockPlayTurnRespondWithError(gameId, cardPlayedIsNotInHandError, statusCode);
+      return mutate(playTurn(gameId, { turn: { cardPlayed: { value: 100, type: 'damage' } } })).then(res => {
         expect(res.data).toBe(null);
-        expect(res.errors[0].message).toBe(databaseError.message);
+        expect(res.errors[0].message).toBe(cardPlayedIsNotInHandError.message);
         expect(res.errors[0].extensions.code).toBe(statusCode);
       });
     });
